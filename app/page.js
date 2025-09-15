3177aa78f77e06115078ab7097c2772a186504c0
Dima <mail.korneev@gmail.com>
2025-09-15 15:46:37 +0200

    init project

---
+++ app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex items-start justify-center h-screen">
+      <h2 className="mt-5 text-4xl text-center">Quotes Next.js application</h2>
+    </div>
+  );
+}
